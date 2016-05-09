81b9fa77e33acadeee1778f32f6c9fd2fef47600
Sam Heather <sam@heather.sh>
2016-05-09 18:10:24 +0100

    Initial Commit

---
+++ Zurich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Zurich
+//
+//  Created by Samuel B Heather on 09/05/2016.
+//  Copyright © 2016 Freedom Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
